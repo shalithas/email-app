875280a1b7b85495288b3c15c6e8e0823fcd7100
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2022-03-28 00:33:15 +0800

    failover and overall integration done

--- src/email/email.controller.js
+++ src/email/email.controller.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const emailService = require('./email.service');
 const mailGunService = require('./mailGun.service');
+const sendGridService = require('./sendGrid.service');
+const helpers = require('../helpers');
 
 router.get('/', (req, res) => {
     res.status(405).send({
@@ -11,16 +13,38 @@ router.get('/', (req, res) => {
 
 router.post('/', async (req, res) => {
     const data = req.body;
+
+    // preparing email lists
+    data.to = emailService.prepareEmailAddresses(data.to);
+    data.cc = emailService.prepareEmailAddresses(data.cc);
+    data.bcc = emailService.prepareEmailAddresses(data.bcc);
+
+    //validating
     if (!emailService.validate(data)) {
         return res.status(500).send({
             errorMessage: "Invalid data"
         });
     }
+    const {
+        to,
+        subject,
+        text,
+        cc,
+        bcc
+    } = data;
 
     try {
-        await mailGunService.send('Testing mail app', 'shalitha89@gmail.com', 'Test', 'test message');
+        await mailGunService.send(to, subject, text, cc, bcc);
     } catch (error) { 
-        console.log(error.response);
+        console.log('Mailgun service failed with error');
+        helpers.error.handleAxios(error);
+        console.log('Retrying with SendGrid');
+        try {
+            await sendGridService.send(to, subject, text);
+        } catch (error) {
+            console.log('SendGrid service failed with error');
+            helpers.error.handleAxios(error);
+        }
     }
 
     res.send({
