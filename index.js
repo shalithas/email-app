567388c180509f428e39e29521f11ceb5ee8186a
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2022-03-27 18:46:10 +0800

    body parser added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const config = require('./src/config');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./src/server')(app);
 
