9fe9785086715fda7fc6004e35792597d3133bdf
Shalitha Senarathne <shalitha.senarathne@vmlyr.com>
2022-03-28 11:13:17 +0800

    test configuration done

---
+++ src/helpers/validations.test.js
@@ -0,0 +1,19 @@
+const validations = require('./validations');
+
+describe('Validation helper module', function () {
+    test('Valid email', () => {
+    expect(validations.email('test.user@test.com')).toBe(true);
+    });
+
+    test('Invalid email', () => {
+    expect(validations.email('test.usertest.com')).toBe(false);
+    });
+
+    test('Valid text', () => {
+    expect(validations.textNotEmpty('Valid text')).toBe(true);
+    });
+
+    test('Invalid text', () => {
+    expect(validations.textNotEmpty('')).toBe(false);
+    });
+});
